75d83d2eb3dda194b2176af006d375069a804d2a
Florian CLAVEL <florian.clavel@live.fr>
2013-03-09 13:34:12 +0100

    Checkstyle Correction (400 Errors -> 60 Now)

--- MJX/src/main/java/json2xml/Json2Xml.java
+++ MJX/src/main/java/json2xml/Json2Xml.java
@@ -2,40 +2,16 @@ package json2xml;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.json.XML;
-import org.json.XMLTokener;
-
-import xml2json.Xml2Json;
 
 public class Json2Xml {
-	
-	public static String conversion(String stringJson){
-		//On récupére l'intérieur des balise question
-		try {
-			return FormatXMLMoodle.check(new JSONObject(stringJson));
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}
-		return "Erreur";
-		/*//Debut de reconversion en XML
-		String xmlOut = "<question>";
 
-		//Pour le trie dans le bon sens
-		ArrayList<String> listForSort = new ArrayList<String>();
-		listForSort.add("name");
-		listForSort.add("questiontext");
-		listForSort.add("answer");
-		
-		//C'est l� dedans qu'il y aura des arrangement � faire
-		for(String it:listForSort){
-			xmlOut += "<"+it+">\n\t"+XML.toString(importedJson.get(it))+"\n<"+it+">";
-		}
-		xmlOut += "</question>";
-		
-		//Code XML depuis JSON
-		System.out.println(xmlOut);
-		return stringJson;*/
-		
-	}
-
+    public static String conversion(final String stringJson) {
+        //On récupére l'intérieur des balise question
+        try {
+            return FormatXMLMoodle.check(new JSONObject(stringJson));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return "Erreur";
+    }
 }
--- MJX/src/main/java/xml2json/Xml2Json.java
+++ MJX/src/main/java/xml2json/Xml2Json.java
@@ -5,41 +5,19 @@ import org.json.JSONObject;
 import org.json.XML;
 
 public class Xml2Json {
-	public static String conversion(String stringXML){
-		try {
-			//Utilisation de la methode statique de XML pour conversion vers JSON
-			JSONObject importedJson = XML.toJSONObject(stringXML);
-			//Recuperation du code JSON
-			String json = importedJson.toString();
-			return importedJson.toString();
-		} catch (JSONException e) {
-			//Erreur
-			System.err.println("Erreur lors de la conversion XML->JSON");
-			System.err.println("Exception :" + e);
-		}
-		return "Erreur";
-		
-	
-		/*//On r�cup�re l'int�rieur des balise question
-		importedJson = (JSONObject) importedJson.get("question");
-
-		//Debut de reconversion en XML
-		String xmlOut = "<question>";
-
-		//Pour le trie dans le bon sens
-		ArrayList<String> listForSort = new ArrayList<String>();
-		listForSort.add("name");
-		listForSort.add("questiontext");
-		listForSort.add("answer");
-		
-		//C'est l� dedans qu'il y aura des arrangement � faire
-		for(String it:listForSort){
-			xmlOut += "<"+it+">\n\t"+XML.toString(importedJson.get(it))+"\n<"+it+">";
-		}
-		xmlOut += "</question>";
-		
-		//Code XML depuis JSON
-		System.out.println(xmlOut);
-		*/
-	}
+    public static String conversion(final String stringXML) {
+        try {
+            //Utilisation de la methode statique de XML 
+            //pour conversion vers JSON
+            JSONObject importedJson = XML.toJSONObject(stringXML);
+            //Recuperation du code JSON
+            String json = importedJson.toString();
+            return importedJson.toString();
+        } catch (JSONException e) {
+            //Erreur
+            System.err.println("Erreur lors de la conversion XML->JSON");
+            System.err.println("Exception :" + e);
+        }
+        return "Erreur";
+    }
 }
